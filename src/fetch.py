72d6f3ca32abf942131337375248c1ce0b70c744
Svante Geisshirt <svante@blackbird.zigzak.net>
2023-02-12 11:56:30 +0100

    New logging module

--- src/fetch.py
+++ src/fetch.py
@@ -1,5 +1,7 @@
 import requests
-from config import *
+import json
+from config import api_key
+from log import setup_logger
 
 # URL samt nøgle til API.
 baseURL = 'https://fm-api.dalux.com/api/v1'
@@ -7,13 +9,47 @@ headers = {
     'X-API-KEY':api_key
 }
 
+# Opretter en log.
+peek_log = setup_logger("peek_log", "peek.log")
+
 def get_workorder(w_id):
     return requests.request("Get", baseURL+'/workorders/'+str(w_id), headers=headers)
 
+# Grundet små spring i ID numre bliver der smukigget på de næste 5. Skulle der findes et
+# returneres dette og programmet kører videre derfra. Starter på 0 da det har vist sig at
+# hvis 2 eller flere workorders bliver oprettet inden for kort tid af hinanden (1-2 sekunder)
+# vil den vi leder efter blive sprunget over.
 def peek(w_id):
-    for i in range (1, 6):
+    for i in range (0, 6):
         response = get_workorder(w_id + i)
         if response.status_code == 200:
+            if (i != 0):
+                peek_log.info("Short peeked " + str(i+1) + " from " + str(w_id-1) + " to " + str(w_id+i))
             return w_id + i
-        
+    return w_id
+
+# Grundet større spring (op mod 1000) i ID numre er `peek()` ikke tilstrækkelig. Derfor
+# bliver alle workorders fundet, sorteret og sammelignet med det ID hoved programmet er
+# nået til.
+def peek_long(w_id):
+    all = requests.request("Get", baseURL + "/workorders", headers=headers)
+    allJSON = json.loads(all.content)
+
+    IDs = []
+    for i in range(0, 100):
+       IDs.append(allJSON['items'][i]['data']['ID'])
+
+    IDs.sort()
+    s = w_id
+    for i in range(0, 100):
+        if IDs[i] > w_id:
+            s = IDs[i]
+            break
+
+    for i in range(w_id, s):
+        r = get_workorder(i)
+        if r.status_code == 200:
+            peek_log.info("Long peeked " + str(i-w_id) + " from " + str(w_id) + " to " + str(i))
+            return i
+
     return w_id
--- src/main.py
+++ src/main.py
@@ -4,26 +4,27 @@ import time
 from parser import *
 from patch import *
 from fetch import *
-import logging
+from log import setup_logger
 
-# Sætter log'en.
-logging.basicConfig(filename='workorder.log', level=logging.INFO, format='%(asctime)s %(message)s')
+# Opretter en log.
+main_log = setup_logger("main", "workorder.log")
 
 # Får workorder som argument.
 workorder_id = int(sys.argv[1])
 
 total_sleep = 0
+peek_counter = 0
 while 1:
     try:
         response = get_workorder(workorder_id)
         responseJSON = json.loads(response.content)
     except:
         continue
-        
+
     if response.status_code == 200:
         total_sleep = 0
         print("\nWorkorder " + str(workorder_id) + " blev fundet.")
-        
+
         # Workorder skal ikke opdateres hvis der allerede findes et EAN / PSP i historikken.
         ean_skal_opdateres = True
         psp_skal_opdateres = True
@@ -33,34 +34,42 @@ while 1:
             psp_skal_opdateres = False
 
         ean = None
-        psp = None 
+        psp = None
         if ean_skal_opdateres:
             ean = find_ean_indmelding(responseJSON)
         if psp_skal_opdateres:
             psp = find_psp_indmelding(responseJSON)
-            
+
         if ean == None and psp == None:
-            print("Der findes intet ean eller psp i indmeldninger.")
-            logging.info("\tWorkorder: " + str(workorder_id) + ", har intet i indmeldninger.")
+            print("Der findes intet EAN eller PSP i indmeldninger.")
+            main_log.info("\tWorkorder: " + str(workorder_id) + ", har intet i indmeldninger.")
         elif ean == None:
-            print("Der findes intet ean i indmeldninger.")
+            print("Der findes intet EAN i indmeldninger.")
             patch_psp(baseURL, headers, workorder_id, psp)
-            logging.info("\tWorkorder: " + str(workorder_id) + ", patched PSP.")
+            main_log.info("\tWorkorder: " + str(workorder_id) + ", patched PSP.")
         elif psp == None:
-            print("Der findes intet psp i indmeldninger.")
+            print("Der findes intet PSP i indmeldninger.")
             patch_ean(baseURL, headers, workorder_id, ean)
-            logging.info("\tWorkorder: " + str(workorder_id) + ", patched EAN.")
+            main_log.info("\tWorkorder: " + str(workorder_id) + ", patched EAN.")
         else:
             patch_ean_psp(baseURL, headers, workorder_id, ean, psp)
-            logging.info("\tWorkorder: " + str(workorder_id) + ", patched EAN & PSP.")
-            
+            main_log.info("\tWorkorder: " + str(workorder_id) + ", patched EAN & PSP.")
+
         workorder_id += 1
     else:
-        print("Sleeping for 30 seconds. Total sleep time: " + str(total_sleep) + " seconds.") 
+        print("Sleeping for 30 seconds. Total sleep time: " + str(total_sleep) + " seconds.")
         time.sleep(30)
         total_sleep += 30
         if (total_sleep % 300 == 0):
-            print("Peeking...")
-            workorder_id = peek(workorder_id)
+            print("Peeking - PC: " + str(peek_counter))
+            if (peek_counter >= 20):
+                workorder_id = peek_long(workorder_id)
+                peek_counter = 0
+            else:
+                old_w = workorder_id
+                workorder_id = peek(workorder_id)
+                if (old_w == workorder_id):
+                    peek_counter += 1
+
             print("Continuing at: " + str(workorder_id))
-            
+
