d03990147a8555211c6f5756c102cf48982d78ae
Svante Geisshirt <svante@blackbird.zigzak.net>
2023-06-28 20:40:47 +0200

    New file name for log

--- src/stats.py
+++ src/stats.py
@@ -1,7 +1,7 @@
 from tabulate import tabulate
 from datetime import datetime, timedelta
 
-with open('workorder.log') as f:
+with open('EAN_PSP.log') as f:
     lines = f.readlines()
 
 def string2date(s):
@@ -18,7 +18,7 @@ def since_hours(h):
     while ((nu - date) < timedelta(hours=h) and (l-w_sum) >= 0):
         w_sum += 1
         split = (lines[l-w_sum]).split(",")
-        
+
         if (len(split) == 3):
             if (split[2][1:13] == 'patched EAN.'):
                 e_sum += 1
@@ -27,7 +27,7 @@ def since_hours(h):
             elif(split[2][1:19] == 'patched EAN & PSP.'):
                 e_sum += 1
                 p_sum += 1
-        
+
         date = string2date(lines[l-w_sum][0:19])
     r_arr.append(w_sum)
     r_arr.append(e_sum)
@@ -39,7 +39,7 @@ col_names = ["", "Seneste time", "Seneste døgn", "Seneste 2 døgn","Seneste uge
 data = [
         ["Workorders scannet", since_hours(1)[0], since_hours(24)[0], since_hours(48)[0], since_hours(168)[0]],
         ["Opdateret EAN", since_hours(1)[1], since_hours(24)[1], since_hours(48)[1], since_hours(168)[1]],
-        ["Opdateret PSP", since_hours(1)[2], since_hours(24)[2], since_hours(48)[2],since_hours(168)[2]] 
+        ["Opdateret PSP", since_hours(1)[2], since_hours(24)[2], since_hours(48)[2],since_hours(168)[2]]
        ]
 
 print(tabulate(data, headers=col_names, tablefmt="fancy_grid"))
