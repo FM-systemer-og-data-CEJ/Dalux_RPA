973ecb85f55cffb39df0e27a0d62851d682a2638
Jonathan <jonathan.mailalias@proton.me>
2024-01-25 14:57:47 +0100

    ændret fra == til is, fjernet ubrugte imports

--- dalux-rpa/RPAs/EAN_PSP.py
+++ dalux-rpa/RPAs/EAN_PSP.py
@@ -1,17 +1,21 @@
 from Utils.parserS import parser, find_ean_indmelding, find_psp_indmelding
 from Utils.log import setup_logger
 from Utils.patch import patch_ean_psp, patch_ean, patch_psp
-from Utils.fetch import baseURL, headers
 
 # Opretter en log.
 EAN_PSP_log = setup_logger("main", "EAN_PSP.log")
 
+
 def EAN_PSP_RPA(w_id, json):
     ean_skal_opdateres = True
     psp_skal_opdateres = True
-    for match in parser.parse("data[*].history[*].lines[?(@.title=='EAN/GLN')]").find(json):
+    for match in parser.parse("data[*].history[*].lines[?(@.title=='EAN/GLN')]").find(
+        json
+    ):
         ean_skal_opdateres = False
-    for match in parser.parse("data[*].history[*].lines[?(@.title=='Omk.sted / PSP')]").find(json):
+    for match in parser.parse(
+        "data[*].history[*].lines[?(@.title=='Omk.sted / PSP')]"
+    ).find(json):
         psp_skal_opdateres = False
 
     ean = None
@@ -21,12 +25,12 @@ def EAN_PSP_RPA(w_id, json):
     if psp_skal_opdateres:
         psp = find_psp_indmelding(json)
 
-    if ean == None and psp == None:
+    if ean is None and psp is None:
         EAN_PSP_log.info("\tWorkorder: " + str(w_id) + ", har intet i indmeldninger.")
-    elif ean == None:
+    elif ean is None:
         patch_psp(w_id, psp)
         EAN_PSP_log.info("\tWorkorder: " + str(w_id) + ", patched PSP.")
-    elif psp == None:
+    elif psp is None:
         patch_ean(w_id, ean)
         EAN_PSP_log.info("\tWorkorder: " + str(w_id) + ", patched EAN.")
     else:
