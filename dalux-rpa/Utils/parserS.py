2996d2bd09a59953a34b3a7514d0733a7677818d
Jonathan <jonathan.mailalias@proton.me>
2024-01-25 15:41:14 +0100

    Moved comments in as docstrings. Added type hints.

--- dalux-rpa/Utils/parserS.py
+++ dalux-rpa/Utils/parserS.py
@@ -1,21 +1,35 @@
 from jsonpath_ng.ext import parser
 
-# Retunere det sidst tilføjet EAN-nummer i historiken, hvis der findes et.
-def find_ean_indmelding(json):
-    eans = []
-    for match in parser.parse("data[*].history[*].lines[?(@.title=='EAN-nummer')]").find(json):
-        eans.append(match.value['value'])
+
+def find_ean_indmelding(json) -> list[str] | None:
+    """
+    Retunere det sidst tilføjet EAN-nummer i historiken, hvis der findes et.
+    Arguments:
+    - json
+    """
+    eans: list[str] = []
+    for match in parser.parse(
+        "data[*].history[*].lines[?(@.title=='EAN-nummer')]"
+    ).find(json):
+        eans.append(match.value["value"])
 
     if len(eans) >= 1:
         return eans[0]
     else:
         return None
 
-# Retunere det sidst tilføjet PSP-nummer i historiken, hvis der findes et.
-def find_psp_indmelding(json):
-    psps = []
-    for match in parser.parse("data[*].history[*].lines[?(@.title=='Omkostningssted eller PSP-nummer')]").find(json):
-        psps.append(match.value['value'])
+
+def find_psp_indmelding(json) -> list[str] | None:
+    """
+    Retunere det sidst tilføjet PSP-nummer i historiken, hvis der findes et.
+    Arguments:
+    - json
+    """
+    psps: list[str] = []
+    for match in parser.parse(
+        "data[*].history[*].lines[?(@.title=='Omkostningssted eller PSP-nummer')]"
+    ).find(json):
+        psps.append(match.value["value"])
 
     if len(psps) >= 1:
         return psps[0]
